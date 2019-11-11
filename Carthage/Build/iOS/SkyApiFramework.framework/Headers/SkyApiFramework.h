195e8741bbae63518914d552f195e1aaf95ce028
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-11-11 23:38:27 +0300

    commit framwork

---
+++ Carthage/Build/iOS/SkyApiFramework.framework/Headers/SkyApiFramework.h
@@ -0,0 +1,19 @@
+//
+//  SkyApiFramework.h
+//  SkyApiFramework
+//
+//  Created by Эмиль Шалаумов on 11.11.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SkyApiFramework.
+FOUNDATION_EXPORT double SkyApiFrameworkVersionNumber;
+
+//! Project version string for SkyApiFramework.
+FOUNDATION_EXPORT const unsigned char SkyApiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SkyApiFramework/PublicHeader.h>
+
+
